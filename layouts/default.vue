603806cbd9e6db88264060078680328dd0c288b6
Nikita <nik.fedyushin@gmail.com>
2022-09-22 19:50:17 +0600

    first push

---
+++ layouts/default.vue
@@ -0,0 +1,63 @@
+<template>
+  <v-app dark>
+    <v-app-bar
+      :clipped-left="clipped"
+      fixed
+      app
+    >
+      <v-btn
+        v-for="item in menuItems"
+        :to="item.code"
+      >
+        <v-toolbar-title v-text="item.title" />
+      </v-btn>
+
+      <v-spacer />
+    </v-app-bar>
+    <v-main>
+      <v-container>
+        <Nuxt />
+      </v-container>
+    </v-main>
+    <v-footer
+      :absolute="!fixed"
+      app
+    >
+      <span>&copy; {{ new Date().getFullYear() }}</span>
+    </v-footer>
+  </v-app>
+</template>
+
+<script>
+export default {
+  name: 'DefaultLayout',
+  data () {
+    return {
+      clipped: false,
+      drawer: false,
+      fixed: false,
+      miniVariant: false,
+      right: true,
+      rightDrawer: false,
+      menuItems: [
+        {
+          title: 'Learn words',
+          code: '/'
+        },
+        {
+          title: 'Your dictionary',
+          code: 'dictionary'
+        }
+      ]
+    }
+  },
+  methods: {
+    go(code) {
+      console.log(code)
+
+      if (code === '') this.$router.replace('')
+      this.$router.push(code)
+    }
+  },
+}
+</script>
---
+++ pages/dictionary.vue
@@ -0,0 +1,81 @@
+<template>
+  <div class="dictionary">
+    <div class="col-12 d-flex justify-center dictionary__control-block control-block">
+      <v-btn
+        class="control-block__button mt-16 d-flex"
+        width="350"
+        height="40"
+        @click="dialog = !dialog"
+      >
+        <v-icon>mdi-plus</v-icon>
+        Add new word
+      </v-btn>
+    </div>
+    <div class="dictionary__list"></div>
+
+    <v-dialog
+      v-model="dialog"
+      max-width="500px"
+    >
+      <v-card>
+        <v-card-text class="d-flex pt-5 pb-0">
+          <v-text-field class="col-6" v-model="dialogWord" label="Your word"></v-text-field>
+          <v-text-field class="col-6" v-model="dialogWordTranslate" label="Word's translate"></v-text-field>
+        </v-card-text>
+
+        <v-card-actions class="pb-4">
+          <v-spacer></v-spacer>
+
+          <v-btn
+            text
+            color="primary"
+            @click="addWord"
+          >
+            Add word
+          </v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-dialog>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'dictionary',
+  data() {
+    return {
+      dialog: false,
+      dialogWord: '',
+      dialogWordTranslate: '',
+      wordsList: [
+        {
+          name: 'abandoned',
+          translate: 'заброшенный',
+        },
+        {
+          name: 'evolve',
+          translate: 'развиваться',
+        },
+        {
+          name: 'involve',
+          translate: 'вовлекать',
+        },
+        {
+          name: 'sage',
+          translate: 'мудрец',
+        }
+      ],
+    }
+  },
+  methods: {
+    addWord(data) {
+      this.dialog = false
+      this.wordsList.unshift(data)
+    },
+  },
+}
+</script>
+
+<style scoped>
+
+</style>
